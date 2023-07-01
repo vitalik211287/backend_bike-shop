2170437adf5acbc9120a589698c7cc1a76559b50
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-07-02 01:09:15 +0300

    gitignore config.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const { DB_HOST } = require("./config");
 mongoose.set("strictQuery", true);
 
 const {
   PORT = 3001,
-  DB_HOST = "mongodb+srv://Vitalik:2656vipO@cluster0.emctnag.mongodb.net/db-contacts?retryWrites=true&w=majority",
+  
 } = process.env;
 
 mongoose
