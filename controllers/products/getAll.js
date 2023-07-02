a48ee3b3882217c53f30e0282dea192846755fee
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-07-02 13:04:16 +0300

    getAll database

--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -1,12 +1,12 @@
-// const Product = require("../../models/product")
-const products = require("../../data/velo.json");
+const Product = require("../../models/product")
+// const products = require("../../data/velo.json");
 
 const getAll = async (req, res) => {
   res.json({
     status: "success",
     code: 200,
     data: {
-      result: products,
+      result: Product,
     },
   });
 };
