f453ec4a5a1d7ab8c3d0073afc7b939255c8e06c
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-07-02 11:34:53 +0300

    getAll

--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -1,13 +1,15 @@
 const Product = require("../../models/product")
 
 const getAll = async (req, res) => {
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      result: Product,
-    },
-  });
+  // res.json({
+  //   status: "success",
+  //   code: 200,
+  //   data: {
+  //     result: Product,
+  //   },
+  // });
+  const result = await Product.find()
+  res.json(result)
 };
 
 module.exports = getAll;
