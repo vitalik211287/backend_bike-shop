5085c39b78b7bfe79b08023bc40e15bf4a0d3f9b
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-07-02 12:52:59 +0300

    removeProduct

--- controllers/products/removeById.js
+++ controllers/products/removeById.js
@@ -6,10 +6,11 @@ const removeById = async (req, res) => {
   if (!removepPoducts) {
     throw HttpError(404, `Product with id=${req.params.id} not found`);
   }
+  const newContacts = products.filter(({ id }) => id !== req.params.contactId);
   res.status(200).json({
     status: "Delete success",
     code: 200,
-    data: removepPoducts,
+    data: newContacts,
   });
 };
 module.exports = removeById;
