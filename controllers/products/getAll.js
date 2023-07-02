9cad869b5946fabc79ca7d357ed945fd72d90eb7
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-07-02 13:29:39 +0300

    Update getAll.js

--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -1,14 +1,16 @@
-const Product = require("../../models/product")
+const Product = require("../../models/product");
 // const products = require("../../data/velo.json");
 
 const getAll = async (req, res) => {
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      result: Product.find(),
-    },
-  });
+  // res.json({
+  //   status: "success",
+  //   code: 200,
+  //   data: {
+  //     result: Product.find(),
+  //   },
+  // });
+  const result = await Product.find();
+  res.json(result);
 };
 
 module.exports = getAll;
