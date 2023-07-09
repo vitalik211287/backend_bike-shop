e0e55d61494284e3f9382b5c862cc08728010367
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-07-09 22:04:14 +0300

    check token middleware authentication

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,8 +1,9 @@
 const { HttpError } = require("../../helpers");
 const { User } = require("../../models/user");
-const jvt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 const {SECRET_KEY} = process.env
+const bcrypt = require("bcryptjs");
+
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -19,7 +20,7 @@ const login = async (req, res) => {
     id: user._id,
   }
 
-  const token = jvt.sign(payload,SECRET_KEY, {expiresIn: "23h"});
+  const token = jwt.sign(payload,SECRET_KEY, {expiresIn: "23h"});
   res.json({
     token,
   });
--- routes/api/products.js
+++ routes/api/products.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const ctrl = require("../../controllers/products");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/product");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/:id",authenticate,  isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:id", isValidId, ctrl.removeById);
+router.delete("/:id", authenticate, isValidId, ctrl.removeById);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put("/:id", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+router.patch("/:id/favorite", authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
 module.exports = router;
